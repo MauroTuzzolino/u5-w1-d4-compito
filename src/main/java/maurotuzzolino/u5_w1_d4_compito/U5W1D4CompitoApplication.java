bfd674bc113a288b2c5dd039a608e2b8859f4b25
MauroTuzzolino <tuzzolinomauro.prog05@gmail.com>
2025-07-17 13:38:48 +0200

    Initial commit

---
+++ src/main/java/maurotuzzolino/u5_w1_d4_compito/U5W1D4CompitoApplication.java
@@ -0,0 +1,13 @@
+package maurotuzzolino.u5_w1_d4_compito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5W1D4CompitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5W1D4CompitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/maurotuzzolino/u5_w1_d4_compito/U5W1D4CompitoApplicationTests.java
@@ -0,0 +1,13 @@
+package maurotuzzolino.u5_w1_d4_compito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W1D4CompitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
